cf653f477941247c3450dcabc33c007c83ebd857
droidecahedron <droidecahedron@gmail.com>
2025-06-11 17:07:51 -0700

    solu

--- src/main.c
+++ src/main.c
@@ -6,6 +6,11 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/settings/settings.h>
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/gatt.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/bluetooth/uuid.h>
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 #define DK_STATUS_LED DK_LED1
@@ -23,6 +28,152 @@ K_MSGQ_DEFINE(adc_msgq, sizeof(struct adc_sample_msg), 8,
 #define BLE_THREAD_STACK_SIZE 1024
 #define BLE_THREAD_PRIORITY 5
 
+// Declaration of custom GATT service and characteristics UUIDs
+#define PMIC_HUB_SERVICE_UUID BT_UUID_128_ENCODE(0x69e5204b, 0x8445, 0x5fca, 0xb332, 0xc13064b9dea2)
+#define BOOST_MV_CHARACTERISTIC_UUID BT_UUID_128_ENCODE(0xB0057000, 0x7ea8, 0x4008, 0xb432, 0xb46096c049ba)
+#define LSLDO_MV_CHARACTERISTIC_UUID BT_UUID_128_ENCODE(0x757D0111, 0xd8ee, 0x4faf, 0x956b, 0xafb01c17d0be)
+
+#define BT_UUID_PMIC_HUB BT_UUID_DECLARE_128(PMIC_HUB_SERVICE_UUID)
+#define BT_UUID_PMIC_HUB_BOOST_MV BT_UUID_DECLARE_128(BOOST_MV_CHARACTERISTIC_UUID)
+#define BT_UUID_PMIC_HUB_LSLDO_MV BT_UUID_DECLARE_128(LSLDO_MV_CHARACTERISTIC_UUID)
+
+#define DEVICE_NAME CONFIG_BT_DEVICE_NAME // from prj.conf
+#define DEVICE_NAME_LEN (sizeof(DEVICE_NAME) - 1)
+
+static const struct bt_le_adv_param *adv_param =
+    BT_LE_ADV_PARAM((BT_LE_ADV_OPT_CONN | BT_LE_ADV_OPT_USE_IDENTITY), /* Connectable advertising and use
+                                                                          identity address */
+                    800,   /* Min Advertising Interval 500ms (800*0.625ms) 16383 max*/
+                    801,   /* Max Advertising Interval 500.625ms (801*0.625ms) 16384 max*/
+                    NULL); /* Set to NULL for undirected advertising */
+
+static struct k_work adv_work;
+static const struct bt_data ad[] = {
+    BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+    BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
+};
+
+static const struct bt_data sd[] = {
+    BT_DATA_BYTES(BT_DATA_UUID128_ALL, PMIC_HUB_SERVICE_UUID),
+};
+
+/*This function is called whenever the Client Characteristic Control Descriptor
+(CCCD) has been changed by the GATT client, for each of the characteristics*/
+static void on_cccd_changed(const struct bt_gatt_attr *attr, uint16_t value)
+{
+    ARG_UNUSED(attr);
+    switch (value)
+    {
+    case BT_GATT_CCC_NOTIFY:
+        break;
+    case 0:
+        break;
+    default:
+        LOG_ERR("Error, CCCD has been set to an invalid value");
+    }
+}
+
+BT_GATT_SERVICE_DEFINE(pmic_hub, BT_GATT_PRIMARY_SERVICE(BT_UUID_PMIC_HUB),
+
+                       BT_GATT_CHARACTERISTIC(BT_UUID_PMIC_HUB_BOOST_MV, BT_GATT_CHRC_NOTIFY, BT_GATT_PERM_READ, NULL,
+                                              NULL, NULL),
+                       BT_GATT_CCC(on_cccd_changed, BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
+
+                       BT_GATT_CHARACTERISTIC(BT_UUID_PMIC_HUB_LSLDO_MV, BT_GATT_CHRC_NOTIFY, BT_GATT_PERM_READ, NULL,
+                                              NULL, NULL),
+                       BT_GATT_CCC(on_cccd_changed, BT_GATT_PERM_READ | BT_GATT_PERM_WRITE), );
+
+// BT globals and callbacks
+struct bt_conn *m_connection_handle = NULL;
+static void adv_work_handler(struct k_work *work)
+{
+    int err = bt_le_adv_start(adv_param, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
+    if (err)
+    {
+        LOG_INF("Advertising failed to start (err %d)", err);
+        return;
+    }
+
+    LOG_INF("Advertising successfully started");
+}
+
+static void advertising_start(void)
+{
+    k_work_submit(&adv_work);
+}
+
+static void recycled_cb(void)
+{
+    LOG_INF("Connection object available from previous conn. Disconnect is "
+            "complete!");
+    advertising_start();
+}
+
+static void connected(struct bt_conn *conn, uint8_t err)
+{
+    if (err)
+    {
+        LOG_WRN("Connection failed (err %u)", err);
+        return;
+    }
+    m_connection_handle = conn;
+    LOG_INF("Connected");
+    dk_set_led_on(BLE_STATE_LED);
+}
+
+static void disconnected(struct bt_conn *conn, uint8_t reason)
+{
+    LOG_INF("Disconnected (reason %u)", reason);
+    m_connection_handle = NULL;
+    dk_set_led_off(BLE_STATE_LED);
+}
+
+struct bt_conn_cb connection_callbacks = {
+    .connected = connected,
+    .disconnected = disconnected,
+    .recycled = recycled_cb,
+};
+
+static void ble_report_boost_mv(struct bt_conn *conn, const uint32_t *data, uint16_t len)
+{
+    const struct bt_gatt_attr *attr = &pmic_hub.attrs[2];
+    struct bt_gatt_notify_params params = {
+        .uuid = BT_UUID_PMIC_HUB_BOOST_MV, .attr = attr, .data = data, .len = len, .func = NULL};
+
+    if (bt_gatt_is_subscribed(conn, attr, BT_GATT_CCC_NOTIFY))
+    {
+        if (bt_gatt_notify_cb(conn, &params))
+        {
+            LOG_ERR("Error, unable to send notification");
+        }
+    }
+    else
+    {
+        LOG_WRN("Warning, notification not enabled for boost mv characteristic");
+    }
+}
+
+static void ble_report_lsldo_mv(struct bt_conn *conn, const uint32_t *data, uint16_t len)
+{
+    const struct bt_gatt_attr *attr = &pmic_hub.attrs[5];
+
+    struct bt_gatt_notify_params params = {
+        .uuid = BT_UUID_PMIC_HUB_LSLDO_MV, .attr = attr, .data = data, .len = len, .func = NULL};
+
+    if (bt_gatt_is_subscribed(conn, attr, BT_GATT_CCC_NOTIFY))
+    {
+        // Send the notification
+        if (bt_gatt_notify_cb(conn, &params))
+        {
+            LOG_ERR("Error, unable to send notification");
+        }
+    }
+    else
+    {
+        LOG_WRN("Warning, notification not enabled for lsldo mv characteristic");
+    }
+}
+
 void ble_write_thread(void)
 {
     struct adc_sample_msg msg;
@@ -33,6 +184,17 @@ void ble_write_thread(void)
         // At this point, msg.channel_mv[0] and msg.channel_mv[1] contain the
         // latest ADC results
         LOG_INF("BLE thread received: Ch0(BOOST)=%d mV, Ch1(LDOLS)=%d mV", msg.channel_mv[0], msg.channel_mv[1]);
+
+        if (m_connection_handle) // if ble connection present
+        {
+            ble_report_boost_mv(m_connection_handle, &msg.channel_mv[0], sizeof(msg.channel_mv[0]));
+            ble_report_lsldo_mv(m_connection_handle, &msg.channel_mv[1], sizeof(msg.channel_mv[1]));
+        }
+        else
+        {
+            LOG_INF("BLE Thread does not detect an active BLE connection");
+        }
+
         k_sleep(BLE_NOTIFY_INTERVAL);
     }
 }
@@ -112,6 +274,22 @@ int main(void)
         LOG_ERR("LEDs init failed (err %d)", err);
         return -1;
     }
+
+    // Setting up Bluetooth
+    err = bt_enable(NULL);
+    if (err)
+    {
+        LOG_ERR("Bluetooth init failed (err %d)", err);
+        return -1;
+    }
+    LOG_INF("Bluetooth initialized");
+    if (IS_ENABLED(CONFIG_SETTINGS))
+    {
+        settings_load();
+    }
+    bt_conn_cb_register(&connection_callbacks);
+    k_work_init(&adv_work, adv_work_handler);
+    advertising_start();
 
     for (;;)
     {
